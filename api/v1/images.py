dfb9c7871d239e9c3f220ede5d42f0ca6cfcd2d6
artyomVB <79898892+artyomVB@users.noreply.github.com>
2022-12-18 15:20:48 +0300

    Add new handlers

--- api/v1/images.py
+++ api/v1/images.py
@@ -2,15 +2,25 @@ import uuid
 import zipfile
 
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse, PlainTextResponse
 
 from bugs_generator.bugs_generation import generateRandomBug
 
 app = FastAPI()
 
 
+@app.get("/main")
+async def root():
+    with open("templates/main.html") as file:
+        html_content = file.read()
+
+    response = HTMLResponse(content=html_content)
+    response.charset = "UTF-8"
+    return response
+
+
 @app.get("/api/v1/generate_images")
-async def root(count: int):
+async def generate_images(count: int):
     for i in range(count):
         generateRandomBug("Example.svg", f"bug_{i}.png")
     file_path = 'reportDir' + str(uuid.uuid4()) + '.zip'
@@ -19,3 +29,9 @@ async def root(count: int):
             myzip.write(f"bug_{i}.png")
     return FileResponse(path=file_path, media_type='application/octet-stream', filename=file_path)
 
+
+@app.get("/api/v1/generate_one_image")
+async def generate_one_image():
+    with open("Example.svg") as img:
+        text = img.read()
+    return PlainTextResponse(content=text)
